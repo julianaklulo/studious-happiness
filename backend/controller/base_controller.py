119d2455f7e690c0d389b49941153590836c976f
Juliana Karoline <julianaklulo@gmail.com>
2021-02-05 12:05:53 -0300

    Refactor base controller

--- backend/controller/base_controller.py
+++ backend/controller/base_controller.py
@@ -1,24 +1,22 @@
 from backend.dao.base_dao import BaseDao
 from backend.models.base_model import BaseModel
 
+
 class BaseController:
     def __init__(self, dao: BaseDao):
         self.__dao = dao()
 
-    def save(self, model: BaseModel) -> None:
-        result = self.__dao.save(model)
-        return result
+    def save(self, model: BaseModel) -> BaseModel:
+        return self.__dao.save(model)
 
-    def read_by_id(self, id: int) -> object:
-        result = self.__dao.read_by_id(id)
-        return result
+    def read_by_id(self, id: int) -> BaseModel:
+        return self.__dao.read_by_id(id)
 
     def read_all(self) -> list:
         result = self.__dao.read_all()
         return result
 
     def update(self, model: BaseModel) -> BaseModel:
-        self.read_by_id(model.id)
         return self.__dao.save(model)
 
     def delete(self, model: BaseModel) -> None:
